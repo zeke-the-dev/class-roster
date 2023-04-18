d6673d57c21fa35c2c4d056a8027653a452dfec3
Ezekiel Nwafor <121586121+zeke-the-dev@users.noreply.github.com>
2023-04-18 16:49:56 -0400

    Add files via upload

--- degree.h
+++ degree.h
@@ -4,9 +4,12 @@
 
 #ifndef CLASS_ROSTER_DEGREE_H
 #define CLASS_ROSTER_DEGREE_H
+//sdfghsrtg
+namespace DegreeProgram{
+    //creation of enumerated data type (requirement c)
+    enum DegreeProgram {SECURITY, NETWORK, SOFTWARE};
 
-//creation of enumerated data type (requirement c)
-enum DegreeProgram {SECURITY, NETWORK, SOFTWARE};
-
+    static const std::string program[] = {"SECURITY", "NETWORK", "SOFTWARE"};
+}
 #endif //CLASS_ROSTER_DEGREE_H
 
